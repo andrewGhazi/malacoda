f10a3562773cb42f41d3aca1ad35e26c2fb3b464
Andrew Ghazi <andghazi@gmail.com>
2018-10-04 04:21:41 -0500

    data documentation

---
+++ R/data.R
@@ -0,0 +1,17 @@
+#' Example MPRA annotation set
+#'
+#' DeepSea v0.94 K562 DNase hypersensitivity estimates for the variants in Ulirsch et
+#' al., Cell, 2016.
+#' @format a data frame containing variant_id and annotation columns
+#' @source \url{deepsea.princeton.edu}
+"u_deepsea"
+
+#' Example MPRA counts from Ulirsch et al., 2016
+#'
+#' MPRA counts for 100 randomly chosen variants from Ulirsch et al., Cell, 2016.
+#' Original URL seems to be down as of 10/2/2018.
+#'
+#' @format a data frame giving variant_id, allele identifier, barcode, and
+#'   sequencing counts for the two DNA libraries and 6 control RNA libraries
+#' @source \url{https://www.bloodgenes.org/RBC_MPRA}
+"umpra_example"
