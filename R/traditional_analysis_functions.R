cac3137b89e68c47624740389c15931403eebb2e
Andrew Ghazi <andghazi@gmail.com>
2018-10-05 03:42:03 -0500

    initial commit

---
+++ R/traditional_analysis_functions.R
@@ -0,0 +1,11 @@
+#### Functions for things like computing activity levels, performing t-tests, etc.
+
+
+compute_activites = function(mpra_data){
+
+}
+
+
+run_activity_tests = function(mpra_activities){
+
+}
