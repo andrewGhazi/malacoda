dc3eb7c4bb0ed1bd3a327f2456eec8785d0cd2d2
Andrew Ghazi <andghazi@gmail.com>
2018-10-26 10:56:11 -0500

    documentation and rlang::.data fixes

--- R/sampler_functions.R
+++ R/sampler_functions.R
@@ -113,49 +113,103 @@ run_crispr_sampler = function() {
 
 }
 
-sample_from_prior = function(prior_df){
+#' Sample from prior
+#'
+#' @description Randomly draw activities levels and transcription shifts from
+#'   the prior
+#' @param prior_df a data frame of the prior for one variant
+#' @param n_iter number of prior simulation draws to pull
+#' @details This function draws \code{n_iter} draws from the prior for one
+#'   variant.
+#' @return a data data frame of activity draws and the simulated transcription
+#'   shift at each draw
+#' @export
+sample_from_prior = function(prior_df, n_iter){
   sim_df = prior_df %>%
-    mutate(allele = case_when(tolower(allele) == 'ref' ~ 'ref',
-                              tolower(allele) != 'ref' ~ 'alt'),
-           draws = map2(alpha_est, beta_est, ~rgamma(5e4, shape = .x, rate = .y))) %>%
-    select(allele, draws) %>%
+    mutate(allele = case_when(tolower(.data$allele) == 'ref' ~ 'ref',
+                              tolower(.data$allele) != 'ref' ~ 'alt'),
+           draws = map2(.data$alpha_est, .data$beta_est,
+                        ~rgamma(n_iter, shape = .x, rate = .y))) %>%
+    select(.data$allele, .data$draws) %>%
     unnest %>%
-    mutate(iter = rep(1:5e4, times = 2)) %>%
-    spread(allele, draws) %>%
-    mutate(sim_ts = alt - ref)
+    mutate(iter = rep(1:n_iter, times = 2)) %>%
+    spread(.data$allele, .data$draws) %>%
+    mutate(sim_ts = .data$alt - .data$ref)
 
   return(sim_df)
 }
 
+#' Summarise prior samples
+#'
+#' @description Compute summary statistics for input prior simulation draws
+#' @param sim_df a data frame of simulation draws
+#' @return a row data frame with columns of summary statistics
+#' @export
 summarise_prior_samples = function(sim_df){
 
   sim_summary = sim_df %>%
-    summarise(mean_prior_ts = mean(sim_ts),
-              sd_prior_ts = sd(sim_ts),
-              prior_ts_hdi = list(coda::HPDinterval(coda::mcmc(sim_ts), prob = .99)),
-              prior_lower_ts = prior_ts_hdi[[1]][1],
-              prior_upper_ts = prior_ts_hdi[[1]][2],
-              prior_is_func = !between(0, prior_lower_ts, prior_upper_ts))
+    summarise(mean_prior_ts = mean(.data$sim_ts),
+              sd_prior_ts = sd(.data$sim_ts),
+              prior_ts_hdi = list(coda::HPDinterval(coda::mcmc(.data$sim_ts), prob = .99)),
+              prior_lower_ts = .data$prior_ts_hdi[[1]][1],
+              prior_upper_ts = .data$prior_ts_hdi[[1]][2],
+              prior_is_func = !between(0, .data$prior_lower_ts, .data$prior_upper_ts))
 
   return(sim_summary)
 }
 
-summarise_one_prior = function(prior_df){
-  prior_samples = sample_from_prior(prior_df)
+#' Summarise one RNA prior
+#'
+#' @description Sample from one RNA prior and compute summary statistics
+#' @param prior_df a single RNA prior
+#' @param n_iter number of prior draws used in simulation
+#' @return a data frame with summary statistics for the input prior
+#' @export
+summarise_one_prior = function(prior_df,
+                               n_iter){
+  prior_samples = sample_from_prior(prior_df, n_iter = n_iter)
 
   prior_summary = summarise_prior_samples(prior_samples)
 
   return(prior_summary)
 }
 
-
+#' Summarise a conditional prior
+#'
+#' @description This function runs Monte Carlo simulations for each variant's
+#'   conditional mean priors.
+#' @param cond_prior a conditional prior object
+#' @param n_cores number of cores for parallelization
+#' @param n_iter number of prior simulation draws
+#' @return a data frame of RNA priors with prior simulation summary statistics
+#' @details This is used to ensure that the prior is not TOO specific. While we
+#'   want the prior distribution to accurately reflect prior beliefs on a given
+#'   variant's effects, it would be undesirable for the prior to pre-specify
+#'   that the variant is functional. If the \code{prior_is_func} column in the
+#'   output of this function is all FALSE, this means that no variant is \emph{a
+#'   priori} functional.
+#'
+#'   If one does get a conditional prior that has variants that are \emph{a
+#'   priori} functional, this can be addressed by increasing the
+#'   \code{min_neighbors} argument in \code{fit_cond_prior}. See the
+#'   documentation of that function for details.
+#'
+#'   Currently this function only simulates the mean parameters as in practice
+#'   the dispersion parameters don't vary systematically between alleles and/or
+#'   variants.
+#' @note simulations for individual variants can be obtained with
+#'   \code{summarise_one_prior()} and \code{sample_from_prior()}
+#' @export
 summarise_cond_prior = function(cond_prior,
-                                n_cores = 1){
+                                n_cores = 1,
+                                n_iter = 5e4){
 
   cond_prior$rna_priors %>%
-    mutate(prior_sim_res = parallel::mclapply(variant_m_prior,
+    mutate(prior_sim_res = parallel::mclapply(.data$variant_m_prior,
                                               summarise_one_prior,
-                                              mc.cores = n_cores)) %>%
-    unnest(... = prior_sim_res) %>%
-    arrange(desc(abs(mean_prior_ts)))
+                                              mc.cores = n_cores,
+                                              n_iter = n_iter)) %>%
+    unnest(... = .data$prior_sim_res) %>%
+    select(-.data$prior_sim_res) %>%
+    arrange(desc(abs(.data$mean_prior_ts)))
 }
