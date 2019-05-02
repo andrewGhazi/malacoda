539861212d737b6782ef6dcbe0b2cb9808bca7da
Andrew Ghazi <arghazi@bcm.edu>
2019-05-02 09:46:17 -0500

    removing bloodgenes URL that has gone down

--- R/data.R
+++ R/data.R
@@ -9,11 +9,11 @@
 #' Example MPRA counts from Ulirsch et al., 2016
 #'
 #' MPRA counts for 25 randomly chosen variants from Ulirsch et al., Cell, 2016.
-#' Original URL seems to be down as of 10/2/2018.
+#' Original URL from the manuscript seems to be down as of 10/2/2018.
 #'
 #' @format a data frame giving variant_id, allele identifier, barcode, and
 #'   sequencing counts for the two DNA libraries and 6 control RNA libraries
-#' @source \url{https://www.bloodgenes.org/RBC_MPRA}
+#' @source \url{https://www.bloodgenes.org}
 "umpra_example"
 
 #' Example output of fit_mpra_model
