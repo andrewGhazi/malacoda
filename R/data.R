28aac6ec3ff2609a0b034dc0260722d12b8b8143
Andrew Ghazi <arghazi@bcm.edu>
2019-06-27 05:16:56 -0500

    dropout_example data

--- R/data.R
+++ R/data.R
@@ -63,3 +63,10 @@
 #' variants from that assay, so conditionl priors derived from the subsetted
 #' example data included in malacoda will be different.
 "cond_prior_example"
+
+#' Example CRISPR dropout screen counts
+#'
+#' Example dropout screen counts for 50 randomly selected genes from "Optimized
+#' libraries for CRISPR-Cas9 genetic screens with multiple modalities", Doench
+#' et al., Nature Communications, 2018
+"dropout_example"
